9451a2d429411afc6cf7155c2b86ea6244e273f5
knopsl <markus.stepanovsky@hotmail.com>
2014-10-20 19:18:19 +0200

    addusers works

--- routes/user.js
+++ routes/user.js
@@ -12,3 +12,26 @@ router.get('/', function(req, res) {
 });
 
 module.exports = router;
+
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    db.collection('nodetest2').insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var userToDelete = req.params.id;
+    db.collection('nodetest2').removeById(userToDelete, function(err, result) {
+        res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
